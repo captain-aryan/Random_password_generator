c293b216565ef0ac28448fbefe2231a42540bc6c
C A P T A 1 N <aryansolpankhi123@gmail.com>
2025-10-06 14:39:02 +0530

    updated

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 import string
 import random
-import argparse
-
-argparse = argparse.ArgumentParser(description="Random Password Generator", usage="python3 pass_gen.py -l LOWERCASE -u UPPERCASE -d NUMERIC -s SPECIAL_CHAR")
-argparse.add_argument("-l","--lowercase",help="Enters the lowercase list")
-argparse.add_argument("-u","--uppercase",help="Enters the uppercase list")
-argparse.add_argument("-d","--numeric",help="Enters the numbers list")
-argparse.add_argument("-s","--special",help="Enters the special characters list")
-
-args = argparse.parse_args()
-lowercase = args.lowercase
-uppercase = args.uppercase
-numbers = args.numbers
-special_char = args.special_char
-
-print(f"[+] ")
 
 class Password:
     def __init__(self, charset, length):
@@ -30,7 +15,7 @@ class Password:
         if ('u' in self.charset):
             self.char_array.append(string.ascii_uppercase)
 
-        if ('d' in self.charset):
+        if ('n' in self.charset):
             self.char_array.append(string.digits)
 
         if ('s' in self.charset):
@@ -42,7 +27,10 @@ class Password:
     
     def generate_password(self):
         print(self.char_array)
-        for i in range(len(self.length)):
+        for i in range(self.length):
             outer_index = random.randrange(0, len(self.char_array))
             inner_index = random.randrange(0, len(self.char_array[outer_index]))
             self.password.append(self.char_array[outer_index] [inner_index])
+
+    def get_password(self):    
+        return ''.join(self.password)
